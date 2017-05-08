ab7a27a7bbc62b30d60a7fdb18c8b1967b56da4a
Gary Taylor <gary.taylor@hismessages.com>
2017-05-08 17:44:52 +0100

    More tidying up

--- lib/rspec_apib/elements/http_headers.rb
+++ lib/rspec_apib/elements/http_headers.rb
@@ -4,13 +4,13 @@ module RSpecApib
     # Represents a collection of http headers in api-elements (http://api-elements.readthedocs.io/en/latest/)
     class HttpHeaders < Base
       def [](key)
-        member = content.find { |h| h.is_a?(Member) && h.content.key?(key) }
+        member = content.find { |http_header| http_header.is_a?(Member) && http_header.content.key?(key) }
         return nil if member.nil?
         member.content[key]
       end
 
       def each_pair
-        content.select { |h| h.is_a?(Member) }.each do |header|
+        content.select { |http_header| http_header.is_a?(Member) }.each do |header|
           yield header.key, header.value
         end
       end
@@ -18,7 +18,7 @@ module RSpecApib
       def keep_if
         results = dup
         results.content = []
-        content.select { |h| h.is_a?(Member) }.each do |header|
+        content.select { |http_header| http_header.is_a?(Member) }.each do |header|
           results.content << header if yield header.key, header.value
         end
         results
